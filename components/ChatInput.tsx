95496a7e3c6b9a1d7138e7015e449df71d295b5e
donggdeng <donggdeng@gmail.com>
2023-02-12 17:30:45 +0800

    implement chat input

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -1,4 +1,7 @@
 import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
+import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 type Props = {
   chatId: string;
@@ -6,16 +9,27 @@ type Props = {
 
 function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
+
   return (
-    <div>
-      <form className="p-5 sapce-x-5 flex-1">
+    <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm focus:outline-none">
+      <form className="p-5 sapce-x-5 flex">
         <input
+          className="bg-transparent focus:outline-none flex-1
+          disabled:cursor-not-allowed disabled:text-gray-300"
+          disabled={!session}
           value={prompt}
           type="text"
           placeholder="Type your message here..."
         />
 
-        <button type="submit">
+        <button
+          disabled={!prompt || !session}
+          type="submit"
+          className="bg-[#11A37F] hover:opacity-50 text-white font-bold
+          px-4 py-2 rounded disabled:bg-gray-300
+          disabled:cursor-not-allowed"
+        >
           <PaperAirplaneIcon className="h-4 w-4 -rotate-45" />
         </button>
       </form>
@@ -26,6 +40,3 @@ function ChatInput({ chatId }: Props) {
 }
 
 export default ChatInput;
-function useState(arg0: string): [any, any] {
-  throw new Error("Function not implemented.");
-}
