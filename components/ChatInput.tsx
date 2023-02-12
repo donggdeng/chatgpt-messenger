20f29ea2fa3e21e5bcce786bada7afd0d22c3e26
donggdeng <donggdeng@gmail.com>
2023-02-12 21:50:32 +0800

    add model selection

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -5,6 +5,8 @@ import { useSearchParams } from "next/navigation";
 import { FormEvent, useState } from "react";
 import { toast } from "react-hot-toast";
 import { db } from "../firebase";
+import ModelSelection from "./ModelSelection";
+import useSWR from "swr";
 
 type Props = {
   chatId: string;
@@ -14,8 +16,9 @@ function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState("");
   const { data: session } = useSession();
 
-  // TODO: useSWR to get model
-  const model = "text-davinci-003";
+  const { data: model } = useSWR("model", {
+    fallbackData: "text-davinci-003",
+  });
 
   const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -98,7 +101,9 @@ function ChatInput({ chatId }: Props) {
         </button>
       </form>
 
-      <div>{/* ModelSelection */}</div>
+      <div className="md:hidden">
+        <ModelSelection />
+      </div>
     </div>
   );
 }
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,18 +1,22 @@
-'use client'
+"use client";
 
 import { collection, orderBy, query } from "firebase/firestore";
 import { useSession, signOut } from "next-auth/react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../firebase";
 import ChatRow from "./ChatRow";
+import ModelSelection from "./ModelSelection";
 import NewChat from "./NewChat";
 
 function SideBar() {
   const { data: session } = useSession();
 
   const [chats, loading, error] = useCollection(
-     session && query(collection(db, 'users', session.user?.email!, 'chats'),
-     orderBy("createdAt", "asc"))
+    session &&
+      query(
+        collection(db, "users", session.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
   );
 
   console.log(chats);
@@ -22,12 +26,20 @@ function SideBar() {
       <div className="flex-1">
         <div>
           <NewChat />
-          <div>
-            {/* ModelSelection */}
+          <div className="hidden sm:inline">
+            <ModelSelection />
+          </div>
+
+          <div className="flex flex-col space-y-2 my-2">
+            {loading && (
+              <div className="animate-pulse text-center text-white">
+                <p>Loading Chats...</p>
+              </div>
+            )}
           </div>
 
           {/* Map through the Chatrow */}
-          {chats?.docs.map(chat => (
+          {chats?.docs.map((chat) => (
             <ChatRow key={chat.id} id={chat.id} />
           ))}
         </div>
@@ -43,7 +55,7 @@ function SideBar() {
         />
       )}
     </div>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
